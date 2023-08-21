f8b99e7283eb0407681bedc6c7b02495b99c7504
qianmozhongheng <141086195+qianmozhongheng@users.noreply.github.com>
2023-08-21 17:56:48 +0800

    Add files via upload

---
+++ BGMController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+    //这个类没有用到=-=
+    public static BGMController _instance;
+    public float Volume=0.6f;
+	// Use this for initialization
+	void Awake () {
+        _instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
